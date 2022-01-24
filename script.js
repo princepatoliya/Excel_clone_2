f4bec3b5a22e0a575b73d4aa58cc833aeeb0c549
Prince Patoliya <princepatoliya852@gmail.com>
2022-01-24 16:28:26 +0530

    store every cell state and add two link properties with header

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let defaultProperties = {
+    text: "",
+    "font-weight": "",
+    "font-style": "",
+    "text-decoration": "",
+    "text-align": "left",
+    "background-color": "#ffffff",
+    "color": "#000000",
+    "font-family": "Calibri",
+    "font-size": "14px"
+}
+
+let cellData = {
+    "Sheet1" : {}
+}
+
+let selectedSheet = "Sheet1";
+let totalSheets = 1;
 $(document).ready(function() {
 
     for (let i = 1; i <= 100; i++) {
@@ -73,20 +91,55 @@ $(document).ready(function() {
                     $(`#row-${rowId}-col-${colId+1}`).addClass("left-cell-selected");
                 }
             }
-
-            $(this).addClass("selected");
-
         }
         else{
             $(".input-cell.selected").removeClass("selected top-cell-selected bottom-cell-selected right-cell-selected left-cell-selected");
-            $(this).addClass("selected");
         }
 
-        console.log(getCodeFromNumber(colId) + rowId);
+        $(this).addClass("selected");
+
+        // console.log(getCodeFromNumber(colId) + rowId);
         $('.cell-name-box').empty();
         $('.cell-name-box').append(getCodeFromNumber(colId) + rowId);
 
+        changeHeader(this);
+
     });
+
+
+    // Too way header change
+
+
+    function changeHeader(element){
+        // console.log(getRowColId(element));
+        [rowId, colId] = getRowColId(element);
+        let cellInfo = defaultProperties;
+
+        if(cellData[selectedSheet][rowId] && cellData[selectedSheet][rowId][colId]){
+            cellInfo = cellData[selectedSheet][rowId][colId];
+        }
+
+        cellInfo["font-weight"] ? $(".icon-bold").addClass("selected") : $(".icon-bold").removeClass("selected");
+        cellInfo["font-style"] ? $(".icon-italic").addClass("selected") : $(".icon-italic").removeClass("selected");
+        cellInfo["text-decoration"] ? $(".icon-underline").addClass("selected") : $(".icon-underline").removeClass("selected");
+
+        let alignment = cellInfo['text-align'];
+        $('.align-icons').removeClass('selected');
+        $(`.icon-align-${alignment}`).addClass('selected');
+
+
+        $('.background-color-picker').val(cellInfo['background-color']);
+        $('.font-color-picker').val(cellInfo['color']);
+
+        $('.font-family-selector').val(cellInfo['font-family']);
+        $('.font-family-selector').css("font-family", cellInfo['font-family']);
+        $('.font-size-selector').val(cellInfo['font-size']);
+
+
+
+    }
+
+
 
     $(".input-cell").dblclick(function(){
         $(".input-cell.selected").removeClass("selected");
@@ -129,39 +182,119 @@ function getRowColId(e){
     return [parseInt(id[1]), parseInt(id[3])];
 }
 
-function updateCell(property, value){
+
+
+function updateCell(property, value, defaultPossible){
     $(".input-cell.selected").each(function(){
         $(this).css(property, value);
+
+        const [rowId, colId] = getRowColId(this);
+
+        if(cellData[selectedSheet][rowId]){
+            if(cellData[selectedSheet][rowId][colId]){
+                cellData[selectedSheet][rowId][colId][property] = value;
+            }
+            else{
+                cellData[selectedSheet][rowId][colId] = {...defaultProperties};
+                cellData[selectedSheet][rowId][colId][property] = value;
+            }
+        }else{
+            cellData[selectedSheet][rowId] = {};
+            cellData[selectedSheet][rowId][colId] = {...defaultProperties};
+            cellData[selectedSheet][rowId][colId][property] = value;
+        }   
+
+        if( (defaultPossible) && (JSON.stringify(cellData[selectedSheet][rowId][colId]) === JSON.stringify(defaultProperties)) ){
+            delete cellData[selectedSheet][rowId][colId];
+            if(Object.keys(cellData[selectedSheet][rowId]).length == 0){
+                delete cellData[selectedSheet][rowId];
+            }
+        }
+
+        console.log(cellData);
     });
+
+
     
 }
 
+
+
 $(".icon-bold").click(function(){
     if($(this).hasClass("selected")){
-        updateCell("font-weight", "")
+        updateCell("font-weight", "", true)
     }
     else{
-        updateCell("font-weight", "bold")
+        updateCell("font-weight", "bold", false)
     }
 });
 
 $(".icon-italic").click(function(){
     if($(this).hasClass("selected")){
-        updateCell("font-style", "")
+        updateCell("font-style", "", true)
     }
     else{
-        updateCell("font-style", "italic")
+        updateCell("font-style", "italic", false)
     }
 });
 
 
 $(".icon-underline").click(function(){
     if($(this).hasClass("selected")){
-        updateCell("text-decoration", "")
+        updateCell("text-decoration", "", true)
     }
     else{
-        updateCell("text-decoration", "underline")
+        updateCell("text-decoration", "underline", false)
     }
 });
 
 
+$('.icon-align-left').click(function(){
+    if(!$(this).hasClass('selected')){
+        updateCell("text-align", 'left', true);
+    }
+
+});
+
+$('.icon-align-center').click(function(){
+    if(!$(this).hasClass('selected')){
+        updateCell("text-align", 'center', true);
+    }
+
+});
+
+$('.icon-align-right').click(function(){
+    if(!$(this).hasClass('selected')){
+        updateCell("text-align", 'right', true);
+    }
+
+});
+
+
+
+$('.color-fill-icon').click(function(){
+    $('.background-color-picker').click();
+});
+
+$('.text-color-fill-icon').click(function(){
+    $('.font-color-picker').click();
+});
+
+$('.font-color-picker').change(function(){
+    updateCell("color", $(this).val());
+});
+
+$('.background-color-picker').change(function(){
+    updateCell("background-color", $(this).val());
+});
+
+
+
+$('.font-family-selector').change(function(){
+    updateCell("font-family", $(this).val(), true);
+    $('.font-family-selector').css("font-family", $(this).val());
+});
+
+$('.font-size-selector').change(function(){
+    updateCell("font-size", $(this).val(), true);
+});
